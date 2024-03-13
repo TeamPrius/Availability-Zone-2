72a7261e2d7f6ba9c0a85cb00e134e53a6d92656
Andrew <andrew.colinet@alum.utoronto.ca>
2024-03-13 14:23:32 -0400

    Added EC2 instance and AMI instance file.

---
+++ ec2_availability_zone_2.tf
@@ -0,0 +1,32 @@
+# EC2 Instance information for Availability Zone 2
+
+# Web Tier Jump Box Availability Zone 2
+
+resource "aws_instance" "jump_tier_availability_zone_2" {
+  ami                         = data.aws_ami.amazonlinux2023.id  # ami
+  instance_type               = "t2.micro"                       # ami instance type
+  subnet_id                   = aws_subnet.public_subnet_2.id    # get public subnet id
+  availability_zone           = "us-east-1b"                     # get availability zone
+  associate_public_ip_address = true                             # gives public ip address
+  vpc_security_group_ids      = [aws_security_group.appsg.id]    # get security group id
+
+  tags = {
+    Name = "Jump Tier Availability Zone 2"  # creates name
+  }
+}
+
+
+# Business Logic Layer Availability Zone 2
+
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.amazonlinux2023.id  # ami
+  instance_type               = "t2.micro"                       # ami instance type
+  subnet_id                   = aws_subnet.private_subnet_3.id   # get public subnet id
+  availability_zone           = "us-east-1b"                     # get availability zone
+  associate_public_ip_address = true                             # gives public ip address
+  vpc_security_group_ids      = [aws_security_group.appsg.id]    # get security group id
+
+  tags = {
+    Name = "Business Logic Layer Availability Zone 2"  # creates name
+  }
+}
